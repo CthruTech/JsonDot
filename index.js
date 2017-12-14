1ca914ee99f9f846011dec8649ff502e5a07c61c
Mark Holtzhausen <Mark.Holtzhausen@media24.com>
2017-12-14 14:05:23 +0200

    WIP

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-class Evented {
+class JsonDot {
     constructor() {
-        this.callbacks = {}
-    }
-
-    getCallbacks(event) {
-        if (!Object.prototype.hasOwnProperty.call(this.callbacks, event)) {
-            this.callbacks[event] = [];
-        }
-        return this.callbacks[event];
-    }
-
-    on(event, callback) {
-        let cbs = this.getCallbacks(event)
-        if (typeof event === 'function') cbs.push(event);
-    }
-
-    trigger(event, data) {
-        let cbs = this.getCallbacks(event);
-        cbs.forEach(cb => {
-            if (typeof cb === 'function') { }
-        })
     }
 }
 
-module.exports = Evented;
+module.exports = JsonDot;
